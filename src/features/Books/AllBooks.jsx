d37d84db06d73464401234a8fb973ca713e9ac13
annawaterhouse <annawaterhouse05@gmail.com>
2023-10-24 13:45:11 -0400

    books rendered

--- src/features/Books/AllBooks.jsx
+++ src/features/Books/AllBooks.jsx
@@ -4,29 +4,31 @@ Fetch the book data from the provided API.
 Users should be able to click on an individual book to navigate to 
 the SingleBook component and view its details. */
 import { useGetBooksQuery } from "./booksSlice"
+import { Link } from "react-router-dom"
 
-function BookCard({ book }) {
+function BookCard({ b }) {
     return (
-      <li>
-        <h3>{book.title}</h3>
-        <img src={book.image} />
-        <Link to={`/books/${book.id}`}>Read More</Link>
-      </li>
+        <li>
+            <img className="book-image" src={b.coverimage} alt={b.title} />
+            <h5>{b.title}</h5>
+            <p className="author">{b.author}</p>
+            <p className="status">{b.available ? "Available" : "Out of Stock"}</p>
+            <Link to={`/books/${b.id}`}>Learn More</Link>
+        </li>
     )
-  }
-
-export default function AllBooks() {
-    const { data, error, isLoading } = useGetBooksQuery();
-    const loadMessage = <p>Loading...</p>;
-    const errMessage = <p>Sorry, we ran into an error...Please try again later.</p>;
-    
-
-   const BookList = (
-        <ul className="book-list">
-            {data.data.map((book) => (
-                <BookCard key={b.id} book={book}/>
-            ))}
-        </ul>
-   ) 
-    return isLoading ? loadMessage : error ? errMessage : BookList;
 }
+export default function AllBooks() {
+    const { data = {} } = useGetBooksQuery();
+    console.log(data.books);
+
+    return (
+        <div>
+            {data.books?.map((b) => (
+                <BookCard key={b.id} b={b} />
+            ))}
+        </div>
+    )
+}
+
+//     return isLoading ? loadMessage : error ? errMessage : BookList;
+// }
