6e0b1a6d8266c3edbd1a22d9c74de530f0488aab
Anna Waterhouse <annawaterhouse05@gmail.com>
2024-02-10 17:27:48 -0500

    navigation styling clean up

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,7 +1,3 @@
-/* TODO - add your code to create a functional React component that 
-renders a navigation bar for the different views in your single page application. 
-You may consider conditionally rendering some options - for example 
-'Login' should be available if someone has not logged in yet. */
 import { NavLink } from "react-router-dom"
 import { logout, selectToken, useGetUserInfoQuery } from "../features/Auth/authSlice"
 import { useSelector, useDispatch } from "react-redux"
@@ -13,14 +9,30 @@ import books from "../assets/books.png"
 function Menu() {
     return (
         <nav>
-            <NavLink to="/"><img className="logo" src={books} alt="logo" /></NavLink>
-            <menu>
-                <li>
-                    <NavLink to="/login">Sign In</NavLink>
-                </li>
-            </menu>
+          <NavLink to="/">
+            <img className="logo" src={books} alt="logo" />
+          </NavLink>
+          <NavLink to="/login">
+            <button className="signin-button">
+              Sign In
+              <div className="icon">
+                <svg
+                  height="24"
+                  width="24"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M0 0h24v24H0z" fill="none"></path>
+                  <path
+                    d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+            </button>
+          </NavLink>
         </nav>
-    )
+      );
 }
 
 
